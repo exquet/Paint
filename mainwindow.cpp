d9cb67c643ac632c683a99dec9b05a52ccb86b6d
exquet <dimatopgg@mail.ru>
2025-02-06 14:15:17 +0500

    bug fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     MainWindow::resize(800, 600);
-    m_image = QImage(800, 600, QImage::Format_RGB32);
+    m_image = QImage(800, 600, QImage::Format_RGB32); // создание холста
     m_image.fill(Qt::white);
 }
 
@@ -18,28 +18,24 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::mousePressEvent(QMouseEvent *event){
-    if(event->button() == Qt::LeftButton){
+    if(event->button() == Qt::LeftButton){ // отслеживание первого нажатия
         m_lastPoint = event->pos();
     }
 }
 
 void MainWindow::mouseMoveEvent(QMouseEvent *event){
-    if(event->button() == Qt::LeftButton){
+    if(event->buttons() == Qt::LeftButton){ // отслеживание изменения положия при нажатом ЛКМ
         QPainter painter(&m_image);
 
-        // Настраиваем перо: черный цвет, толщина линии 3 пикселя, сплошная линия с закругленными концами
+        // перо: черный цвет, толщина линии 3 пикселя, сплошная линия с закругленными концами
         painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-        // Рисуем линию от последней сохраненной точки до текущей позиции курсора
-        painter.drawLine(m_lastPoint, event->pos());
-        // Обновляем m_lastPoint для непрерывного рисования
-        m_lastPoint = event->pos();
-        // Запрашиваем перерисовку окна, чтобы новые линии отобразились
-        update();
+        painter.drawLine(m_lastPoint, event->pos()); // отрисовка линии от начального положения до текущего
+        m_lastPoint = event->pos(); // обновление положения курсора
+        update(); // обновление холста
     }
 }
 
 void MainWindow::paintEvent(QPaintEvent *event){
     QPainter painter(this);
-    // Рисуем наше изображение (холст) в верхнем левом углу окна (0,0)
-    painter.drawImage(0, 0, m_image);
+    painter.drawImage(0, 0, m_image); // отрисовка изображения
 }
